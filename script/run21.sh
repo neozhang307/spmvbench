6cc63772d9b79803e13efc92755780a82bd24a53
lingqi.zhang <lingqi.zhang@riken.jp>
2024-12-11 00:13:46 +0900

    update benchmark bash

--- script/run21.sh
+++ script/run21.sh
@@ -8,8 +8,8 @@ input="./data/matrix_list21.csv"
   while IFS=',' read -r mid Group Name rows cols nonzeros
   do
     echo "$mid $Group $Name $rows $cols $nonzeros"
-    ./spmv_double  $MM_path/$Group/$Name/$Name.mtx 0
-    ./spmv_half  $MM_path/$Group/$Name/$Name.mtx 0
+    ./spmv_double  $MM_path/$Group/$Name/$Name.mtx 1
+#    ./spmv_half  $MM_path/$Group/$Name/$Name.mtx 0
     i=`expr $i + 1`
   done 
 } < "$input"
